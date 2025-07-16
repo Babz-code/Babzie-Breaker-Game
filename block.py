1a8c32da7afb54e7924bfee89583903811631482
Toyosi Adeniyi <adetbabz@gmail.com>
2025-07-16 09:43:49 +0100

    Added colors to the block

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 class Block:
     def __init__(self):
@@ -6,10 +7,11 @@ class Block:
         self.x = -265
         self.y = 65
         self.more_blocks = 0
+        self.block_color = ["red", "green", "blue", "cyan", "lightgreen",  "turquoise", "skyblue"]
         while self.more_blocks < 5:
             for _ in range(1, 10):
                 new = Turtle("square")
-                new.color("white")
+                new.color(random.choice(self.block_color))
                 new.penup()
                 new.shapesize(2, 3)
                 new.goto(self.x, self.y)
